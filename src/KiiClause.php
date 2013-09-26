76cf23c85e799a3130f3246d2d802364623624ef
hrk fkm <fkm@fkmsoft.jp>
2013-09-26 18:05:25 +0900

    Add Clauses

--- src/KiiClause.php
+++ src/KiiClause.php
@@ -10,6 +10,89 @@ class KiiClause {
 		return new KiiClause('all');
 	}
 
+	public static function equals($field, $value) {
+		$c = new KiiClause('eq');
+		$c->clause['field'] = $field;
+		$c->clause['value'] = $value;
+		
+		return $c;		
+	}
+	
+	public static function greaterThan($field, $value, $include) {
+		$c = new KiiClause('range');
+		$c->clause['field'] = $field;
+		$c->clause['lowerLimit'] = $value;
+		$c->clause['lowerIncluded'] = $include;		
+		
+		return $c;		
+	}
+
+	public static function lessThan($field, $value, $include) {
+		$c = new KiiClause('range');
+		$c->clause['field'] = $field;
+		$c->clause['upperLimit'] = $value;
+		$c->clause['upperIncluded'] = $include;		
+		
+		return $c;		
+	}
+
+	public static function range($field, $fromValue, $fromInclude,
+								 $toValue, $toInclude) {
+		$c = new KiiClause('range');
+		$c->clause['field'] = $field;
+		$c->clause['lowerLimit'] = $fromValue;
+		$c->clause['lowerIncluded'] = $fromInclude;		
+		$c->clause['upperLimit'] = $toValue;
+		$c->clause['upperIncluded'] = $toInclude;		
+		
+		return $c;		
+	}
+	
+	public static function inClause($field, array $values) {
+		$c = new KiiClause('in');
+		$c->clause['field'] = $field;
+		$c->clause['values'] = $values;
+		
+		return $c;		
+	}
+	
+	public static function not(KiiClause $clause) {
+		$c = new KiiClause('not');
+		$c->clause['clause'] = $clause->toJson();
+		
+		return $c;		
+	}
+	
+	public static function andClause() {
+		$c = new KiiClause('and');
+		$array = KiiClause::toFlatArray(func_get_args());
+		$c->clause['clauses'] = $array;
+		
+		return $c;
+	}
+
+	public static function orClause() {
+		$c = new KiiClause('or');
+		$array = KiiClause::toFlatArray(func_get_args());
+		$c->clause['clauses'] = $array;
+
+		return $c;		
+	}
+	
+	private static function toFlatArray($args) {
+		$array = array();
+		foreach ($args as $arg) {
+			if (is_array($arg)) {
+				foreach ($arg as $a) {
+					$array[]= $a->toJson();
+				}
+			} else {
+				$array[]= $arg->toJson();
+			}
+		}
+		return $array;
+	}
+
 	public function toJson() {
 		return $this->clause;
 	}
--- test/TestKiiClause.php
+++ test/TestKiiClause.php
@@ -2,6 +2,11 @@
 require_once (dirname(__FILE__) . '/../src/KiiClause.php');
 
 class TestKiiClause extends PHPUnit_Framework_TestCase{
+	private function assertAllClause($clause) {
+		$this->assertEquals(1, count($clause));
+		$this->assertEquals('all', $clause['type']);
+	}
+	
 	public function test_0000_all() {
 		$clause = KiiClause::all();
 		
@@ -9,6 +14,177 @@ class TestKiiClause extends PHPUnit_Framework_TestCase{
 		$json = $clause->toJson();
 		$this->assertEquals(1, count($json));
 		$this->assertEquals('all', $json['type']);
+	}
+
+	public function test_0010_and() {
+		$clause = KiiClause::andClause(
+									   KiiClause::all(), KiiClause::all());
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(2, count($json));
+		$this->assertEquals('and', $json['type']);
+		$clauses = $json['clauses'];
+		$this->assertEquals(2, count($clauses));
+		$this->assertAllClause($clauses[0]);
+		$this->assertAllClause($clauses[1]);		
+	}
+
+	public function test_0011_and_array() {
+		$a = array(KiiClause::all(),
+				   KiiClause::all());
+		$clause = KiiClause::andClause($a);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(2, count($json));
+		$this->assertEquals('and', $json['type']);
+		$clauses = $json['clauses'];
+		$this->assertEquals(2, count($clauses));
+		$this->assertAllClause($clauses[0]);
+		$this->assertAllClause($clauses[1]);		
+	}
+
+	public function test_0020_or() {
+		$clause = KiiClause::orClause(
+									   KiiClause::all(), KiiClause::all());
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(2, count($json));
+		$this->assertEquals('or', $json['type']);
+		$clauses = $json['clauses'];
+		$this->assertEquals(2, count($clauses));
+		$this->assertAllClause($clauses[0]);
+		$this->assertAllClause($clauses[1]);		
+	}
+
+	public function test_0021_or_array() {
+		$a = array(KiiClause::all(),
+				   KiiClause::all());
+		$clause = KiiClause::orClause($a);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(2, count($json));
+		$this->assertEquals('or', $json['type']);
+		$clauses = $json['clauses'];
+		$this->assertEquals(2, count($clauses));
+		$this->assertAllClause($clauses[0]);
+		$this->assertAllClause($clauses[1]);		
+	}
+
+	public function test_0030_not() {
+		$clause = KiiClause::not(KiiClause::all());
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(2, count($json));
+		$this->assertEquals('not', $json['type']);
+		$clauses = $json['clause'];
+		$this->assertAllClause($clauses);
+	}
+
+	public function test_0040_in() {
+		$clause = KiiClause::inClause("age", array(1, 2, 3));
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(3, count($json));
+		$this->assertEquals('in', $json['type']);
+		$this->assertEquals('age', $json['field']);
+		$values = $json['values'];
+		$this->assertEquals(3, count($values));
+		$this->assertEquals(1, $values[0]);
+		$this->assertEquals(2, $values[1]);
+		$this->assertEquals(3, $values[2]);	
+	}
+
+	public function test_0050_greaterThan_false() {
+		$clause = KiiClause::greaterThan("age", 18, false);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(4, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['lowerLimit']);
+		$this->assertFalse($json['lowerIncluded']);		
+	}
+
+	public function test_0051_greaterThan_true() {
+		$clause = KiiClause::greaterThan("age", 18, true);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(4, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['lowerLimit']);
+		$this->assertTrue($json['lowerIncluded']);		
+	}
+
+	public function test_0060_lessThan_false() {
+		$clause = KiiClause::lessThan("age", 18, false);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(4, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['upperLimit']);
+		$this->assertFalse($json['upperIncluded']);		
+	}
+
+	public function test_0061_lessThan_true() {
+		$clause = KiiClause::lessThan("age", 18, true);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(4, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['upperLimit']);
+		$this->assertTrue($json['upperIncluded']);		
+	}
+
+	public function test_0070_range_false() {
+		$clause = KiiClause::range("age", 18, false, 60, false);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(6, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['lowerLimit']);
+		$this->assertFalse($json['lowerIncluded']);
+		$this->assertEquals(60, $json['upperLimit']);
+		$this->assertFalse($json['upperIncluded']);		
+	}
+
+	public function test_0071_range_true() {
+		$clause = KiiClause::range("age", 18, true, 60, true);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(6, count($json));
+		$this->assertEquals('range', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['lowerLimit']);
+		$this->assertTrue($json['lowerIncluded']);
+		$this->assertEquals(60, $json['upperLimit']);
+		$this->assertTrue($json['upperIncluded']);		
+	}
+
+	public function test_0080_equals() {
+		$clause = KiiClause::equals("age", 18);
+		
+		// assertion
+		$json = $clause->toJson();
+		$this->assertEquals(3, count($json));
+		$this->assertEquals('eq', $json['type']);
+		$this->assertEquals('age', $json['field']);		
+		$this->assertEquals(18, $json['value']);
 	}
 }
 ?>
