3635bc186bdbff2fbe5ebf19358dbba802aa3c02
hrk fkm <fkm@fkmsoft.jp>
2013-12-25 16:39:09 +0900

    Add demo of Delete bucket

--- demo/Constants.php
+++ demo/Constants.php
@@ -4,5 +4,7 @@ define ('APP_ID', '<input app ID here>');
 define ('APP_KEY', '<input app Key here>');
 // set Server location here
 define ('SITE', KiiContext::SITE_JP);
+define ('CLIENT_ID', '<input app clinent ID');
+define ('CLIENT_SECRET', '<input app clinent ID');
 
 ?>
