fc6b93273c833c392989fc1a73d2c25f5d476a97
hrk fkm <fkm@fkmsoft.jp>
2014-02-24 14:11:32 +0900

    Add some fields for APNs

--- src/KiiAPNsMessage.php
+++ src/KiiAPNsMessage.php
@@ -2,26 +2,42 @@
 
 class KiiAPNsMessage {
 	public $data;
+    private $alert;
 	private $enable;
-	
-	public function __construct() {
-		$this->data = array();
-		$this->enable = TRUE;
-	}
+    private $badge;
+    
+    public function __construct() {
+        $this->data = array();
+        $this->alert = array();
+        $this->enable = TRUE;
+        $this->badge = 0;
+    }
 
-	public function setEnabled($value) {
-		$this->enable = $value;
-	}	
+    public function setEnabled($value) {
+        $this->enable = $value;
+    }
 
-	public function toJson() {
-		$json = array(
-					  "enabled" => $this->enable
-					  );
-		if (count($this->data) > 0) {
-			$json['data'] = $this->data;
-		}
+    public function setBadge($value) {
+        $this->badge = $value;
+    }
 
-		return $json;
-	}
+    public function setBody($value) {
+        $this->alert['body'] = $value;
+    }
+
+    public function toJson() {
+        $json = array(
+                      "enabled" => $this->enable,
+                      'badge' => $this->badge
+                      );
+        if (count($this->data) > 0) {
+            $json['data'] = $this->data;
+        }
+        if (count($this->alert) > 0) {
+            $json['alert'] = $this->alert;
+        }        
+
+        return $json;
+    }
 }
 ?>
